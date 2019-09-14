0352d2a6b85a90d4d7fb962171cd921cf25b8604
Dalton Menezes <daltonmenezes@outlook.com>
2019-09-14 11:15:18 -0300

    Add renderOnRepoTabBoxesFooter function

---
+++ src/ui/renderer/render-on-repo-tab-boxes-footer.js
@@ -0,0 +1,46 @@
+import createSVG from '../../utils/svg/create-svg'
+
+export default props =>
+  Array.from(document.querySelectorAll('#user-repositories-list ul li'),
+    element => {
+      const repoPath = element.children[0].children[0].querySelector('h3 a').href
+      const wrapper = document.createElement('a')
+
+      wrapper.classList.add('pinned-item-meta', 'muted-link', props.className)
+      wrapper.href = `${ repoPath }/${props.category}`
+
+      const svg = createSVG({ dPath: props.svg.dPath })
+
+      svg.classList.add('octicon', props.svg.className)
+
+      wrapper.prepend(svg)
+
+      const span = document.createElement('span')
+
+      span.classList.add('ml-0', 'mr-3')
+      span.append(wrapper)
+
+      const elementFooter = element
+        .children[0]
+        .lastElementChild
+        .querySelector('relative-time')
+        .previousElementSibling
+
+
+        const licenseItem =
+          elementFooter
+            ? elementFooter.querySelector('span > svg[class*="octicon-law"]')
+            : ''
+
+        if (!licenseItem) {
+            elementFooter
+              ? elementFooter.insertAdjacentElement('afterend', span)
+              : element.children[0].lastElementChild.prepend(span)
+
+            return
+        }
+
+        licenseItem
+          ? licenseItem.insertAdjacentElement('beforebegin', span)
+          : ''
+  })

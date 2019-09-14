8ea35b0aac36dc4f117f0d36fd03ef14a67a91b8
Dalton Menezes <daltonmenezes@outlook.com>
2019-09-14 18:57:24 -0300

    Add isTagsPage

--- src/utils/is/index.js
+++ src/utils/is/index.js
@@ -1,9 +1,11 @@
 import { isFirefox } from './is-firefox'
 import { isFeedPage } from './pages/is-releases-page'
 import { isReleasesPage } from './pages/is-releases-page'
+import { isTagsPage } from './pages/is-tags-page'
 
 export {
   isFirefox,
   isFeedPage,
-  isReleasesPage
+  isReleasesPage,
+  isTagsPage
 }

cf819b8925bac11a7da22a107e490eaef112012a
Dalton Menezes <daltonmenezes@outlook.com>
2019-09-14 12:34:47 -0300

    Refactor svg properties object moving it to svg/config file  and overriding only necessary properties in caller functions

--- src/ui/issues/render-issues-on-feed.js
+++ src/ui/issues/render-issues-on-feed.js
@@ -1,15 +1,8 @@
-import { SVGDPath } from './svg-d-path'
+import config from './svg/config'
 import renderOnFeedBoxesFooter from '../renderer/render-on-feed-boxes-footer'
 
 export default () =>
   renderOnFeedBoxesFooter({
-    category: 'issues',
-    className: 'issues-feed-icon',
-    svg: {
-      className: 'octicon-git-issue-opened',
-      dPath: SVGDPath,
-      viewBox: '0 0 14 16',
-      width: '14',
-      height: '16',
-    }
+    ...config,
+    className: 'issues-feed-icon'
   })
---
+++ src/ui/issues/svg/config.js
@@ -0,0 +1,13 @@
+import { SVGDPath } from './svg-d-path'
+
+export default {
+  category: 'issues',
+  className: 'issues-feed-icon',
+  svg: {
+    className: 'octicon-git-issue-opened',
+    dPath: SVGDPath,
+    viewBox: '0 0 14 16',
+    width: '14',
+    height: '16',
+  }
+}
